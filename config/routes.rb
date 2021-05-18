8221120e299b462281217da4f3b7ab04b43e62df
Julian Thompson <j.thompson@hotmail.de>
2021-05-18 12:50:05 +0200

    added task_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'tasks', to: 'tasks#index'
+  get 'tasks', to: 'tasks#index', as: :tasks
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  get 'tasks', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
 end
